84277dfb8a4bd4d27825f7bceb7005cbeb2b8d0b
charlesagyemang <micnkru@gmail.com>
2017-02-11 14:36:19 +0000

    Feat: Added functionalities to the login sign out to lead to the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
+
+
+
+  get 'userdash/index'
+
+  devise_for :users, controllers: { registrations: "registrations" }
   root 'welcome#index'
   get 'about/index'
   get 'contact/index'
