362c8aa67cbcd7871027626bac91e4bdbbc53b89
charlesagyemang <micnkru@gmail.com>
2017-02-11 12:17:31 +0000

    Feat; Sign Up na Sign inImplemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'welcome#index'
   get 'about/index'
   get 'contact/index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
