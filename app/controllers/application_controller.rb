ef2fc16b63b9c28a09b08df611a1a8bb5c3c5806
charlesagyemang <micnkru@gmail.com>
2017-02-13 01:12:06 +0000

    Final work on the homepage and login redirection and other stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    :userdash_index
+  end
+
+  def after_sign_in_path_for(resource)
+    :userdash_index
+  end
+  
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def after_sign_up_path_for(resource)
-    :userdash_index
-  end
-
+  # def after_sign_up_path_for(resource)
+  #   :userdash_index
+  # end
+  #
+  # def after_sign_in_path_for(resource)
+  #   :userdash_index
+  # end
 end
