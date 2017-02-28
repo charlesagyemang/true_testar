fafd336d0d0b63dc7f3c1c7d57c4948db6701bec
charlesagyemang <micnkru@gmail.com>
2017-02-28 00:18:48 +0000

    css fixing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
-  def index
-   @user = current_user
-   @tests = Test.all
-  end
+    def index
+     @user = current_user
+     @tests = Test.all
+     if user_signed_in? || testowner_signed_in?
+      if current_user
+        redirect_to userdash_index_path
+      else
+        redirect_to testowners_profile_path
+      end
+     end
+    end
 end
